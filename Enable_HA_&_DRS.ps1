a5c6e9dea221131539bcf31b8e87fda9416ce528
Gokul T <76623691+mr-tgokul@users.noreply.github.com>
2025-03-24 21:55:59 +0530

    Enable HA and DRS

---
+++ Enable_HA_&_DRS.ps1
@@ -0,0 +1,23 @@
+﻿# Script to Disable HA and DRS in the Given Logical Datacenter
+
+
+# Specify the Datacenter name
+$datacenterName = "XXXXXX"
+
+$datacenter = Get-Datacenter -Name $datacenterName
+$clusters = Get-Cluster -Location $datacenter
+
+if ($clusters) {
+    foreach ($cluster in $clusters) {
+        Write-Host "Enabling HA and DRS on cluster: $($cluster.Name)" -ForegroundColor Yellow
+        # Enable HA
+        Set-Cluster -Cluster $cluster -HAEnabled:$true -Confirm:$false
+        # Enable DRS
+        Set-Cluster -Cluster $cluster -DrsEnabled:$true -Confirm:$false
+
+        Write-Host "HA and DRS have been Enabled on cluster: $($cluster.Name)" -ForegroundColor Green
+    }
+}
+else {
+    Write-Host "No clusters found in datacenter '$datacenterName'." -ForegroundColor Red
+}
